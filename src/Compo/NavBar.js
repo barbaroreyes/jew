c91705e6a227d629098c16a246b1ae17f8181639
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-27 10:35:55 -0500

    added-mui

--- src/Compo/NavBar.js
+++ src/Compo/NavBar.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const Container =  styled.div`
+height: 60px;
+`
+
+const  Wrapper =  styled.div`
+ padding: 10px 20px ;
+ border: 2px solid black ;
+ display:flex;
+ justify-content: space-between;
+`
+const Left =  styled.div`
+ padding: 10px 20px 
+`
+const Rigth =  styled.div`
+ padding: 10px 20px 
+`
+const Center =  styled.div`
+ padding: 10px 20px 
+`
 
 const NavBar = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <Container>
+        <Wrapper>
+         <Left>leftt</Left>
+         <Center>center</Center>
+         <Rigth>Rigr</Rigth>
+        
+        </Wrapper>
+      
+    </Container>
   );
 }
 
